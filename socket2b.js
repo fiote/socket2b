ef8ab9d76791583f57528eb0fa86f6e5231b9afe
fiote <murilo.eng@gmail.com>
2020-09-10 12:13:49 -0300

    base script

---
+++ socket2b.js
@@ -0,0 +1,75 @@
+class Socket2b {
+
+	cbid = 0;
+	open = false;
+	listeners = [];
+	callbacks = {};
+	internals = ['open','close','error'];
+
+	constructor(ws) {
+		this.ws = ws;
+
+		this.internals.forEach(method => {
+			this.ws.addEventListener(method, ev => {
+				this.trigger(method, ev.data);
+				if (method == 'open') this.setOpen();
+				if (method == 'close') this.setClosed();
+			});
+		});
+
+		this.ws.addEventListener('message',ev => {
+			const { channel, cbid, data } = this.parseEvent(ev);
+			if (channel == 'callback') return this.parseCallback(cbid, data);
+			this.trigger(channel, data);
+		});
+	}
+
+	setOpen() {
+		this.open = true;
+	}
+
+	setClosed() {
+		this.open = false;
+		this.callbacks = [];
+		this.listeners = [];
+	}
+
+	parseEvent(ev) {
+		let feed = {};
+		try {
+			feed = JSON.parse(ev.data);
+		} catch(e) {
+			feed = {channel: 'global', data: {event:'message', message:ev.data}};
+		}
+		return feed;
+	}
+
+	parseCallback(cbid, data) {
+		const callback = this.callbacks[cbid];
+		delete this.callbacks[cbid];
+		if (callback) callback(data);
+	}
+
+	trigger(channel, data) {
+		this.listeners.forEach(list => {
+			if (list.channel === channel) list.callback(data);
+		});
+	}
+
+	emit(channel, data, callback) {
+		const message = {channel, data};
+		if (callback) {
+			message.cbid = this.cbid++;
+			 this.callbacks[message.cbid] = callback;
+		}
+		this.ws.send(JSON.stringify(message));
+	}
+
+	on(channel, callback) {
+		this.listeners.push({channel, callback});
+	}
+
+	close() {
+		this.ws.close();
+	}
+}
